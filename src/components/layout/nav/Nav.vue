e2ef5ee5969c7d1f96210eba6c3e74b879aabea2
Daniel <daniellosada17@gmail.com>
2023-07-25 12:47:58 -0500

    working on it - nav: added hamburger menu on navbar page, responsive web design also added

--- src/components/layout/nav/Nav.vue
+++ src/components/layout/nav/Nav.vue
@@ -10,16 +10,48 @@
     </div>
     <nav class="menu-options">
       <ol>
-        <router-link class="led-effect" :to="{path: '/', exact: true}">Home</router-link>
-        <router-link class="led-effect" :to="{path: '/about-me', exact: true}">About-Me</router-link>
-        <router-link class="led-effect" :to="{path: '/cv', exact: true}">CV</router-link>
-        <router-link class="led-effect" :to="{path: '/web-portfolio', exact: true}">Web-Portfolio</router-link>
-        <router-link class="led-effect" :to="{path: '/contact-me', exact: true}">Contact-Me</router-link>
+        <router-link class="led-effect" :to="{ path: '/', exact: true }"
+          >Home</router-link
+        >
+        <router-link class="led-effect" :to="{ path: '/about-me', exact: true }"
+          >About-Me</router-link
+        >
+        <router-link class="led-effect" :to="{ path: '/cv', exact: true }"
+          >CV</router-link
+        >
+        <router-link class="led-effect" :to="{ path: '/web-portfolio', exact: true }"
+          >Web-Portfolio</router-link
+        >
+        <router-link class="led-effect" :to="{ path: '/contact-me', exact: true }"
+          >Contact-Me</router-link
+        >
       </ol>
     </nav>
 
+    <nav class="second-menu-options" :class="{ open: isOpen }">
+      <button class="btn-close-second-menu" type="button" @click="toggleSecondMenu()">
+        X
+      </button>
+      <ol>
+        <router-link class="led-effect" :to="{ path: '/', exact: true }"
+          >Home</router-link
+        >
+        <router-link class="led-effect" :to="{ path: '/about-me', exact: true }"
+          >About-Me</router-link
+        >
+        <router-link class="led-effect" :to="{ path: '/cv', exact: true }"
+          >CV</router-link
+        >
+        <router-link class="led-effect" :to="{ path: '/web-portfolio', exact: true }"
+          >Web-Portfolio</router-link
+        >
+        <router-link class="led-effect" :to="{ path: '/contact-me', exact: true }"
+          >Contact-Me</router-link
+        >
+      </ol>
+    </nav>
     <div class="container-button-bar">
-      <button class="btn-bar" type="button">
+      <button class="btn-bar" type="button" @click="toggleSecondMenu()">
         <img :src="barsIcon" alt="Menu" />
       </button>
     </div>
@@ -30,15 +62,43 @@
 import { RouterLink } from "vue-router";
 import barsIcon from "../../../assets/icons/nav/bars-icon-menu.png";
 import profileImg from "../../../assets/profile.png";
+import { onBeforeUnmount, onMounted, ref } from "vue";
 
 export default {
   name: "Nav",
   props: ["smallNavbar"],
 
   setup(props) {
+    const isOpen = ref(false);
+
+    const toggleSecondMenu = () => {
+      isOpen.value = !isOpen.value;
+    };
+
+    const handleScroll = () => {
+      if (window.scrollY < 20) {
+        isOpen.value = false;
+      } else if (window.screenY == 0) {
+        isOpen.value = false;
+      }
+    };
+
+// ---------------------------------------------------------------------
+    onMounted(() => {
+      window.addEventListener("scroll", handleScroll);
+    });
+
+
+    onBeforeUnmount(() => {
+      window.removeEventListener("scroll", handleScroll);
+    });
+
+
     return {
       barsIcon,
       profileImg,
+      isOpen,
+      toggleSecondMenu,
     };
   },
 };
