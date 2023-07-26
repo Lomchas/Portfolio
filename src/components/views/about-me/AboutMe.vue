b98024445c159d186041b0f5b29e323ddf5b7f97
Daniel <daniellosada17@gmail.com>
2023-07-25 20:08:25 -0500

    working on it - Features: Presentation letter text was added on about-me page, some styles and layouts was modificated to fix a few responsive web design bugs

--- src/components/views/about-me/AboutMe.vue
+++ src/components/views/about-me/AboutMe.vue
@@ -2,22 +2,26 @@
   <div class="container-aboutMe">
     <div class="wrapper-aboutMe">
       <div class="container-illustration">
-        <img :src="illustration1" alt="illustration">
+        <img :src="illustration1" alt="illustration" />
       </div>
       <div class="container-info">
         <h2 class="title">¡About Me!</h2>
         <hr />
         <p class="info">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur laboriosam
-          exercitationem saepe necessitatibus dolorum! A, quos eum. Exercitationem velit
-          ipsam, est animi ipsa doloribus nam voluptates repellendus culpa nobis itaque
-          rerum harum quod delectus maxime dignissimos perferendis omnis vel neque ullam
-          dolor earum at consequuntur asperiores! Deserunt, doloremque? Autem minima
-          placeat consequuntur assumenda dolorum odit cupiditate minus. Temporibus
-          veritatis inventore quod nemo eius. Pariatur facilis voluptas impedit, fugiat
-          non magnam commodi nihil et est nisi maiores nemo tempora quibusdam sit
-          accusamus corrupti sapiente voluptate reiciendis, porro libero accusantium esse
-          adipisci. Odio cumque culpa, quia incidunt non eius quidem? Ea, mollitia!
+          Hey there! I'm Daniel Losada, a tech and innovation enthusiast born in Cali,
+          Colombia. My mind works in grids, making problem-solving a creative breeze. Web
+          design and layout are my jam, but I also rock the backend scene. Every year, I'm
+          all about leveling up my game, aiming to be a whiz in cybersecurity or data
+          science. When I'm not deep into the digital realm, you'll find me chillin' in
+          rural spots, vibing with nature and showing mad love to animals. I'm a total
+          social butterfly, living for the diversity of people and experiences. Oh, and
+          you'll never catch me without a cap on my head—it's like an essential part of my
+          style! Flexibility is my superpower—both in my fingers and adapting to
+          challenges with a calm, analytical mindset. I call myself a "knowledge vacuum"
+          'cause I'm always thirsty for learning and evolving in programming. My dream is
+          to make tech safer and more efficient for everyone. Excited about the future and
+          all the rad opportunities coming my way! Thanks for stopping by my portfolio.
+          Let's team up on some mind-blowing projects, shall we? Cheers!
         </p>
         <div class="container-btns">
           <router-link to="/contact-me">Hire Me!</router-link>
@@ -29,18 +33,15 @@
 </template>
 
 <script>
-import illustration1 from '../../../assets/illustrations/illustration1.png'
+import illustration1 from "../../../assets/illustrations/illustration1.png";
 
 export default {
-  setup(){
-
-
-    return{
+  setup() {
+    return {
       illustration1,
-    }
-  }
-}
-
+    };
+  },
+};
 </script>
 
 <style lang="sass">
