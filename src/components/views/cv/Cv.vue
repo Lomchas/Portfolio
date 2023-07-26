a6be3ffe5b0b8db7cd7a4886515c01b50e360148
Daniel <daniellosada17@gmail.com>
2023-07-26 17:17:52 -0500

    working on it - fixes: the cv url file was chaged, a link was added to a new download feature to Cv

--- src/components/views/cv/Cv.vue
+++ src/components/views/cv/Cv.vue
@@ -8,8 +8,9 @@
     </div>
     <hr />
     <div class="wrapper-cv">
-      <embed zoom="90" class="cv" src="/assets/docs/cv.pdf" type="application/pdf" />
+      <embed zoom="90" class="cv" src="https://drive.google.com/file/d/1gTnDNGrq9WKTh8bzN4PHSRLb_dzJ2wWz/preview" type="application/pdf" />
     </div>
+    <a class="download-btn" href="https://drive.google.com/file/d/1gTnDNGrq9WKTh8bzN4PHSRLb_dzJ2wWz/view" target="_blank"> Download it here!</a>
   </div>
 </template>
 
