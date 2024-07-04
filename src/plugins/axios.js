6b24ec43443fadd643a10a556351fe3ee95b7e88
Daniel Losada <daniellosada17@gmail.com>
2024-07-04 15:05:41 -0500

    Installation: Axios library was added to project

---
+++ src/plugins/axios.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+
+const axiosInstance = axios.create({
+    baseURL: 'https://portfolio-backend-1-yhze.onrender.com/portfolioDaniellosada'
+})
+
+
+export default axiosInstance;
