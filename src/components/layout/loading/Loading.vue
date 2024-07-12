3eb2830eb9a48cbe70a379f49a6eb58cd0b2cc3f
Daniel Losada <daniellosada17@gmail.com>
2024-07-12 13:35:48 -0500

    fix: Fixing route to src/components/layout/loading/Loading.vue on App.vue

--- src/components/layout/loading/Loading.vue
+++ src/components/layout/loading/Loading.vue
@@ -1,16 +1,17 @@
 <template>
-  <div class="container-loading">
-    <img src="../../../assets/icons/Loading/loading.svg" alt="Loading..." />
-    Loading...
-    <span>Please wait a few seconds, I'm using a free hosting so i have to wait 50 secs for the first request. Beforehand thanks a lot!</span>
-  </div>
-</template>
-
-<script>
-export default {
-};
-</script>
-
-<style lang="sass">
-@import './styles/loading.scss'
-</style>
+    <div class="container-loading">
+      <img src="../../../assets/icons/Loading/loading.svg" alt="Loading..." />
+      Loading...
+      <span>Please wait a few seconds, I'm using a free hosting so i have to wait 50 secs for the first request. Beforehand thanks a lot!</span>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+  };
+  </script>
+  
+  <style lang="sass">
+  @import './styles/loading.scss'
+  </style>
+  
